0170527ee208073477f24a7612764ecc17f2a7e1
michael <michaelignalaga@gmail.com>
2020-09-17 03:27:53 +0800

    dashblade

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,13 +1,88 @@
 @extends('layouts.index')
 
 @section('content')
-           
-    
+<div class="justify-content-center">
+<div class="container">
 
-<div class="container" >
+    <!-- <div class="row"> -->
+
+        <div class="col-md-6 col-sm-6">
+              <div class="x_panel tile overflow_hidden">
+                <div class="x_title">
+                  <h2>Energy consumption per Month</h2>
+                  <!-- <ul class="nav navbar-right panel_toolbox" >
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                  </ul> -->
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <div style="height:230px">
+                        {!!  $LineChart->container() !!}
+                        {!! $LineChart->script() !!}
+                    </div>
+                </div>
+              </div>
+        </div>
+            <div class="col-md-4 col-sm-4 ">
+              <div class="x_panel tile overflow_hidden">
+                <div class="x_title">
+                  <h2>Subscriber Status</h2>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <div style="height:230px">
+                        {!!  $PieChart->container() !!}
+                        {!! $PieChart->script() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-4 ">
+              <div class="x_panel tile overflow_hidden">
+                <div class="x_title">
+                  <h2>Energy consumption per Month</h2>
+                  <!-- <ul class="nav navbar-right panel_toolbox" >
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                  </ul> -->
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <div style="height:230px">
+                        {!!  $BarChart->container() !!}
+                        {!! $BarChart->script() !!}
+                    </div>
+                </div>
+              </div>
+        </div>
+        <div class="col-md-6 col-sm-6 ">
+              <div class="x_panel tile overflow_hidden">
+                <div class="x_title">
+                  <h2>Number of Power Cuts per Month</h2>
+                  <!-- <ul class="nav navbar-right panel_toolbox" >
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                  </ul> -->
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <div style="height:230px">
+                        {!!  $powerCuts->container() !!}
+                        {!! $powerCuts->script() !!}
+                    </div>
+                </div>
+              </div>
+        </div>
+    <!-- </div> -->
+  </div>
+</div>
+
+<!-- <div class="container" >
             <div class="row">
                 <div class="col-xl-8">
                     <div class="header1">
+
                         <table class="table table-striped">
                             <thead>
                                 <tr>List of Accounts Due Today</tr>
@@ -43,7 +118,7 @@
                     <div class="container" style="background color: rgb(192, 192, 192)">
                         <div class="row">
                             <div class="col-xl-12"  style= "color: white;">
-                                <h5 style="margin-top: 20px; color: white;">Notifications</h5> 
+                                <h5 style="margin-top: 20px; color: white;">Notifications</h5>
                                     @foreach($notifications as $notification)
                                         <tr class="table-success">
                                             <td style="text-align: right;"><img src="/images/CHE.png" style="width: 40px; height: 40px;"> Account Number  <strong>{{$notification->accountnum}}</strong></td>
@@ -55,7 +130,7 @@
                                             <td  style="text-align: left;">{{$notification->created}}</td>
                                             <td> ------------------------------------</td>
                                         </tr>
-                                    @endforeach  
+                                    @endforeach
                             </div>
                         </div>
                     </div>
@@ -77,7 +152,7 @@
                                 <tr>
                                     <td>Account Number</td>
                                     <td>Account Name</td>
-                                    <!-- <td>Meter Number</td> -->
+                                    <td>Meter Number</td>
                                     <td>Address</td>
                                     <td>Category</td>
                                     <td>Post Type</td>
@@ -93,7 +168,7 @@
                                 <tr>
                                     <td>{{$account->account_number}}</td>
                                     <td>{{$account->account_name}}</td>
-                                    <!-- <td>{{$account->meter_number}}</td> -->
+                                    <td>{{$account->meter_number}}</td>
                                     <td>{{$account->address}}</td>
                                     <td>{{$account->category}}</td>
                                     <td>{{$account->post_type}}</td>
@@ -108,10 +183,10 @@
                     <div class="container" style="background color: rgb(192, 192, 192)">
                         <div class="row">
                             <div class="col-xl-12"  style= "color: white;">
-                                <a style="color: white;" href="/userschart">View Charts</a> 
+                                <a style="color: white;" href="/userschart">View Charts</a>
                             </div>
                         </div>
-                    </div> 
+                    </div>
                 </div>
             </div>
             <div class="row">
@@ -121,7 +196,7 @@
                     </form>
                 </div>
             </div>
-        </div>
-        
-    
+        </div> -->
+
+
 @endsection
